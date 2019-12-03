8502c1ad398b2c291f33fa650f7e421f6b4230f1
Alexander Görtz <nylocx@gmail.com>
2019-12-03 19:53:24 +0100

    Update day 3 solution.
    A more elegant version of my initial approach. This could be made a lot more memory efficient by just storing the line segments and computing intersections. My idea would be to keep different sets for horizontal and vertical segments and than check the horizontals of the first path against the verticals of the second and vice versa.
    Step computation than can use the sum of all segment length up to the intersecting segments and than adding the remaining steps of the intersecting segments.

--- day_03.py
+++ day_03.py
@@ -1,7 +1,6 @@
 # %% Imports
 from __future__ import annotations
 
-from collections import defaultdict
 from dataclasses import dataclass
 
 
@@ -14,67 +13,32 @@ class Point:
     def distance_to(self, p: Point) -> int:
         return abs(p.x - self.x) + abs(p.y - self.y)
 
+    def __add__(self, other: Point) -> Point:
+        return self.__class__( self.x + other.x, self.y + other.y)
+
     def __hash__(self):
         return hash((self.x, self.y))
 
-    def __iter__(self):
-        yield self.x
-        yield self.y
 
-
-def add_to_grids(path_num, step_num, position):
-    pos_grid[pos].add(path_num)
-    if pos not in steps_grid[path_num]:
-        steps_grid[path_num][position] = step_num
-
-
-# %% Part 1 and Part 2
-pos_grid = defaultdict(set)
-steps_grid = defaultdict(dict)
-origin = Point(0, 0)
+#%% Better solution for Part 1 and 2
 with open("day_03.input", "r") as input_data:
-    for i, path_line in enumerate(input_data):
-        c_x, c_y = origin
-        j = 1
-        for step in path_line.strip().split(","):
-            direction, value = step[0], int(step[1:]) + 1
-            if direction == "R":
-                for x in range(c_x + 1, c_x + value):
-                    pos = Point(x, c_y)
-                    add_to_grids(i, j, pos)
-                    j += 1
-                c_x = x
-            elif direction == "D":
-                for y in range(c_y - 1, c_y - value, -1):
-                    pos = Point(c_x, y)
-                    add_to_grids(i, j, pos)
-                    j += 1
-                c_y = y
-            elif direction == "L":
-                for x in range(c_x - 1, c_x - value, -1):
-                    pos = Point(x, c_y)
-                    add_to_grids(i, j, pos)
-                    j += 1
-                c_x = x
-            elif direction == "U":
-                for y in range(c_y + 1, c_y + value):
-                    pos = Point(c_x, y)
-                    add_to_grids(i, j, pos)
-                    j += 1
-                c_y = y
-            else:
-                print("Wrong input!")
+    wires = [l.strip().split(",") for l in input_data]
 
-pos_grid[origin] = set()
-distance = None
-steps = None
-for point, paths in pos_grid.items():
-    d = point.distance_to(origin)
-    if len(paths) > 1 and (distance is None or d < distance):
-        distance = d
-        s = sum(steps_grid[path][point] for path in paths)
-        if steps is None or s < steps:
-            steps = s
+deltas = {"U": Point(0, 1), "R": Point(1, 0), "D": Point(0, -1), "L": Point(-1, 0)}
+paths = []
+origin = Point(0, 0)
+for wire in wires:
+    path = {}
+    current_pos = origin
+    steps = 1
+    for move in wire:
+        for _ in range(int(move[1:])):
+            current_pos += deltas[move[0]]
+            path[current_pos] = steps
+            steps += 1
+    paths.append(path)
 
-print(f"Part 1: Closes distance is {distance}")
-print(f"Part 2: Minimum steps are {steps}")
+xing = set.intersection(set(paths[0].keys()), set(paths[1].keys()))
+print(f"Part 1: Closes distance is {min(i.distance_to(origin) for i in xing)}")
+print(f"Part 2: Minimum steps are {min(paths[0][i] + paths[1][i] for i in xing)}")
+
